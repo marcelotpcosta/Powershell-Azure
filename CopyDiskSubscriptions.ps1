312dc7121d82c0d484b13c1710975474dcafc70c
Marcelo T P Costa <marcelotpcosta@outlook.com>
2018-06-28 18:04:05 -0300

    Update CopyDiskSubscriptions.ps1

--- CopyDiskSubscriptions.ps1
+++ CopyDiskSubscriptions.ps1
@@ -1,11 +1,11 @@
 #Provide the subscription Id of the subscription where managed disk exists
-$sourceSubscriptionId='cdd5afd6-c1c6-4c56-8c77-edf30c4e0472'
+$sourceSubscriptionId=''
 
 #Provide the name of your resource group where managed disk exists
-$sourceResourceGroupName='RG-SharePointComercial'
+$sourceResourceGroupName=''
 
 #Provide the name of the managed disk
-$managedDiskName='EXDADGNSR402-OSDisk_Caso_00127628'
+$managedDiskName=''
 
 #Set the context to the subscription Id where Managed Disk exists
 Select-AzureRmSubscription -SubscriptionId $sourceSubscriptionId
@@ -15,10 +15,10 @@ $managedDisk= Get-AzureRMDisk -ResourceGroupName $sourceResourceGroupName -DiskN
 
 #Provide the subscription Id of the subscription where managed disk will be copied to
 #If managed disk is copied to the same subscription then you can skip this step
-$targetSubscriptionId='fd3362a2-9173-419e-a06e-db6282bd9a2f'
+$targetSubscriptionId=''
 
 #Name of the resource group where snapshot will be copied to
-$targetResourceGroupName='Sharepoint_Desenvolvimento'
+$targetResourceGroupName=''
 
 #Set the context to the subscription Id where managed disk will be copied to
 #If snapshot is copied to the same subscription then you can skip this step
